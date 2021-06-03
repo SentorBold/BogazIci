2f10b6c433f486ab7d04e0fd5329aef8046dff02
SentorBold <aliumut68@hotmail.com>
2021-06-03 17:54:17 +0300

    31 denemesi

--- BogazIci/Assets/NEW.cs
+++ BogazIci/Assets/NEW.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class deneme : MonoBehaviour
+public class NEW : MonoBehaviour
 {
+    public string denemeler;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class deneme : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(denemeler);
     }
 }
