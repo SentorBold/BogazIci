3018f72d31f96317fc669f8c4f7411d91cff243c
SentorBold <aliumut68@hotmail.com>
2021-06-03 22:14:10 +0300

    aas

--- BogazIci/Assets/NEW.cs
+++ BogazIci/Assets/NEW.cs
@@ -8,7 +8,7 @@ public class NEW : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        iננננ
     }
 
     // Update is called once per frame
