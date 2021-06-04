2fe1bd9d57a5d6a2c0d86d112be3794b70eaff4d
Rakun665 <yemir.yilmaz2@gmail.com>
2021-06-05 02:09:11 +0300

    Coking beta 2

--- BogazIci/Assets/Codes/Cookin.cs
+++ BogazIci/Assets/Codes/Cookin.cs
@@ -6,50 +6,159 @@ public class Cookin : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Sos"))
-        {
-            Debug.Log("Piþ");
+        if (other.CompareTag("Domates"))
+        { 
+            Debug.Log("Pis");
         }
-        if (other.CompareTag("Sebze"))
+
+        if (other.CompareTag("Patlican"))
         {
-            Debug.Log("Piþ");
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Havuc"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Makarna"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Tursu"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Elma"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Armut"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Mantar"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Limon"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Biber"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Tuz"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Ketcap"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Mayonez"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Kolonya"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Ekmek"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Cig kofte"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Aci sos"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Salatalik"))
+        {
+            Debug.Log("Pis");
         }
         if (other.CompareTag("Et"))
         {
-            Debug.Log("Piþ");
+            Debug.Log("Pis");
         }
-        if (other.CompareTag("Baharatlar"))
+        if (other.CompareTag("Olu et"))
         {
-            Debug.Log("Piþ");
+            Debug.Log("Pis");
         }
-        if (other.CompareTag("Hamur iþ"))
+        if (other.CompareTag("Kan"))
         {
-            Debug.Log("Piþ");
+            Debug.Log("Pis");
         }
-        if (other.CompareTag("Ýðrenç"))
+        if (other.CompareTag("Balik"))
         {
-            Debug.Log("Piþ");
+            Debug.Log("Pis");
         }
-        if (other.CompareTag("Kýzartmalar"))
+        if (other.CompareTag("Beyin"))
         {
-            Debug.Log("Piþ");
+            Debug.Log("Pis");
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if (other.CompareTag("Yarasa"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Zeytinyagi"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Yumurta"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Cheddar"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Nutella"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Kekik"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Tozbiber"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Pul biber"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Zerdecal"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Zencefil"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Tarcin"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Karabiber"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Kimyon"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Barbeku sos"))
+        {
+            Debug.Log("Pis");
+        }
+        if (other.CompareTag("Kuru nane"))
+        {
+            Debug.Log("Pis");
+        }
     }
         
 }
