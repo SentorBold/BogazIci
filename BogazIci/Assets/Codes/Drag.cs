b1dfdfbcb6217730d2d1fe1ef283caa213dcde72
Rakun665 <yemir.yilmaz2@gmail.com>
2021-06-05 18:08:32 +0300

    Drag update

--- BogazIci/Assets/Codes/Drag.cs
+++ BogazIci/Assets/Codes/Drag.cs
@@ -3,33 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Drag : MonoBehaviour, IPointerDownHandler, IBeginDragHandler , IEndDragHandler, IDragHandler
+public class Drag : MonoBehaviour // IPointerDownHandler, IBeginDragHandler , IEndDragHandler, IDragHandler
 {
-    
+    Vector3 previouspos;
+    public float sens;
 
-    private Transform transform;
+    /* private Transform transform;
 
-    private void Awake()
+     private void Awake()
+     {
+         transform = GetComponent<Transform>();
+     }
+     public void OnBeginDrag(PointerEventData evenData)
+     {
+         print("Worked");
+     }
+     public void OnDrag(PointerEventData evenData)
+     {
+         print("Worked");
+         transform.position += (Vector3)evenData.delta; // canvas.scaleFactor;
+     }
+     public void OnEndDrag(PointerEventData evenData)
+     {
+         print("");
+     }
+     public void OnPointerDown(PointerEventData evenData)
+     {
+         print("x");
+     }  */
+    private void OnMouseDrag()
     {
-        transform = GetComponent<Transform>();
+        var current = Input.mousePosition;
+        var delta = current - previouspos;
+        transform.position += delta * sens;
+
+        previouspos = current;
     }
-    public void OnBeginDrag(PointerEventData evenData)
+    private void OnMouseDown()
     {
-        print("OnBeingDrag");
+        previouspos = Input.mousePosition;
     }
-    public void OnDrag(PointerEventData evenData)
+
+    private void OnMouseEnter()
     {
-        print("OnDrag");
-        transform.position += (Vector3)evenData.delta; // canvas.scaleFactor;
+        
     }
-    public void OnEndDrag(PointerEventData evenData)
-    {
-        print("OnEndDrag");
-    }
-    public void OnPointerDown(PointerEventData evenData)
-    {
-        print("x");
-    }
-  
-  
 }
