91c22ecfa1e87cb4a663411e5c96dff06c117d01
kkcifors <kutaykarahan6@gmail.com>
2021-06-06 12:43:30 +0300

    proje yetişmedi g.o

---
+++ Assets/choosedFood.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class choosedFood : MonoBehaviour
+{
+    public GameObject place1;
+    public GameObject place2;
+    public GameObject place3;
+    public GameObject place4;
+    public GameObject place5;
+    public GameObject place6;
+
+    public bool choosen0;
+    public bool choosen1;
+    public bool choosen2;
+    public bool choosen3;
+    public bool choosen4;
+    public bool choosen5;
+    public bool choosenCucumber;
+    public bool choosenTomato;
+    public bool choosenPickles;
+    public bool choosenEggplant;
+    public bool choosenMeat;
+    public bool choosenMushroom;
+    public bool choosenLemon;
+    public bool choosenSalt;
+    public bool choosenApple;
+    public bool choosenFish;
+    public bool choosenBrain;
+    public bool choosenBat;
+    public bool choosenMayonnaise;
+
+    public GameObject cucumber;
+    public GameObject tomato;
+    public GameObject pickles;
+    public GameObject eggplant;
+    public GameObject meat;
+    public GameObject mushroom;
+    public GameObject lemon;
+    public GameObject salt;
+    public GameObject apple;
+    public GameObject fish;
+    public GameObject brain;
+    public GameObject bat;
+    public GameObject mayonnaise;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
