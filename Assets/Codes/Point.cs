e2f570cb89818e0c405544446629f89fdb560917
Rakun665 <yemir.yilmaz2@gmail.com>
2021-06-06 15:00:13 +0300

    x

--- Assets/Codes/Point.cs
+++ Assets/Codes/Point.cs
@@ -1,25 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Point : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text max;
+    public bool max1;
+    public Text min;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (gameObject.tag.Equals("Yarasa"))
+        {
+
+            max.enabled = true;
+
+          }
+       else if (gameObject.tag.Equals("Beyin"))
+        {
+            max.enabled = true;
+        }
+        else
+        {
+            min.enabled = true;
+
+
+        }
+
+
+       
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (gameObject.layer.Equals("Guzel"))
-        {
-            print("x");
-        }
-    }  
+
+
+
 }
